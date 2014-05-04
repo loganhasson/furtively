bdb2006e8b9fbb979f32111b99d0aa123dd6b1e7
Logan Hasson <logan.hasson@gmail.com>
2014-05-04 17:43:32 -0400

    Use editable div instead of form

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,25 +1,39 @@
 $(function(){
+  var newMessageInput = $('div#new-message');
 
-  $('#new-message').on('submit', function(event){
-    var message = $('#new-message-text').val();
-    $.post('http://107.170.152.141:9080/pub?id=furtively', message, function(data){
+  function setUpNewMessage() {
+    newMessageInput.attr('contentEditable', true);
+
+    newMessageInput.keypress(function(event) {
+      if (event.which == 13) {
+        submitMessage(newMessageInput.text());
+        event.preventDefault();
+      };
     });
-    
-    event.preventDefault();
-  });
-  
+  };
+
+  function setUpPushStream() {
+    var pushstream = new PushStream({
+      host: '107.170.152.141',
+      port: 9080,
+      modes: 'eventsource'
+    });
+
+    pushstream.onmessage = messageReceived;
+    pushstream.addChannel('furtively');
+    pushstream.connect();
+  };
+
+  function submitMessage(message) {
+    $.post('http://107.170.152.141:9080/pub?id=furtively', message, function(data) {
+      newMessageInput.text("");
+    });
+  };
+
   function messageReceived(text, id, channel) {
     $('section#messages').prepend('<div class="message"><p>' + text + '</p></div>');
   };
-
-  var pushstream = new PushStream({
-    host: '107.170.152.141',
-    port: 9080,
-    modes: 'longpolling'
-  });
-
-  pushstream.onmessage = messageReceived;
-  pushstream.addChannel('furtively');
-  pushstream.connect();
-
+  
+  setUpNewMessage();
+  setUpPushStream();
 });
